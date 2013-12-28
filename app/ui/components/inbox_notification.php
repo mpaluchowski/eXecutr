f1a9cd353562b06fa35d7028316ed3b2cb98026f
Michal Paluchowski <michal@paluchowski.com>
2013-12-28 09:52:01 +0100

    Showing the inbox count even if 0.

--- app/ui/components/inbox_notification.php
+++ app/ui/components/inbox_notification.php
@@ -1,8 +1,5 @@
 <a href="main/process_inbox" title="<?php echo Base::instance()->get('lang.InboxNotificationTitle') ?>"><?php 
 	echo Base::instance()->get('lang.InboxNotificationLabel');
-	if ($inboxItems != 0):
 ?>
 	<span id="inbox-count"><?php echo $inboxItems ?></span>
-<?php
-	endif;
-?></a>
+</a>
